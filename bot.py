c848a35afa037ef96cce81f92544abd3ea40ed12
Shaman457 <zak.artemm2@gmail.com>
2021-08-01 09:05:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 session = vk_api.VkApi(token="960bde19ae45ba97070e6e4bd45f5a34f58c6be4e6a1ce57c321e65a94fc657b026e1f7a582dc5e300109")
-longpoll = VkLongPoll(session, 206207722)
+longpoll = VkBotLongPoll(session, 206207722)
 
 def send_message(chat_id, message):
 	session.method("message.send",{
