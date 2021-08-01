d5bb76b079d47007f01ff5235c76c89905cee224
Shaman457 <zak.artemm2@gmail.com>
2021-08-01 09:18:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.longpoll import VkLongPoll, VkEventType
 session = vk_api.VkApi(token="960bde19ae45ba97070e6e4bd45f5a34f58c6be4e6a1ce57c321e65a94fc657b026e1f7a582dc5e300109")
-longpoll = VkBotLongPoll(session, 206207722)
 
-def send_message(chat_id, message):
+def send_message(user_id, message):
 	session.method("message.send",{
-		"chat_id": chat_id,
+		"user_id": user_id,
 		"message": message,
 		"random_id": 0
 	})
 
-for event in longpoll.listen():
-	if event.type == VkEventType.MASSAGE_NEW:
-		if event.from_chat:
-			id = event.chat_id
-			msg = event.object.nessage['message'].lower()
-			if msg == 'hello':
-				sender(id, 'privet')
+for event in VkLongPoll(session).listen():
+	if event.type == VkEventType.MASSAGE_NEW and event.to_me:
+		text = event.text.lower()
+		user_id = event.chat_id
+
+		if text == "hello":
+			send_message(user_id, "Text")
